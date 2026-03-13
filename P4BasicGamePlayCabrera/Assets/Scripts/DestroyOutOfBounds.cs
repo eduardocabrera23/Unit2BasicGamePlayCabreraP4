cbbf9ec6cf3864dc9ffebd43daa1352c929757d6
eduardocabrera23 <768550@student.dusd.net>
2026-03-13 11:49:32 -0700

    on last lesson need to finish last part of  last lesson.

--- P4BasicGamePlayCabrera/Assets/Scripts/DestroyOutOfBounds.cs
+++ P4BasicGamePlayCabrera/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,6 +4,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
 
     private float topBound = 30;
+    private float lowerBound = -10;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +17,8 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.z > topBound)
         {
+            Destroy(gameObject);
+        } else if (transform.position.z  < lowerBound) {
             Destroy(gameObject);
         }
     }
